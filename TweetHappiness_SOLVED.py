04981b944b36aefcbd23f93385ca7b598525b52b
david880110 <34464522+david880110@users.noreply.github.com>
2018-03-24 10:39:11 -0700

    fix apikey

--- TweetHappiness_SOLVED.py
+++ TweetHappiness_SOLVED.py
@@ -6,7 +6,10 @@ import json
 import random
 
 # Twitter API Keys
-from config import consumer_key, consumer_secret, access_token, access_token_secret
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
 
 # Quotes to Tweet
 happy_quotes = [
